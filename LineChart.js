3af16ee9dfde11668d2691f9f80234adb93782a9
bipinemp <bhandaribipin3.39@gmail.com>
2023-05-23 22:18:34 +0545

    first commit

---
+++ LineChart.js
@@ -0,0 +1,127 @@
+"use client";
+import { Line } from "react-chartjs-2";
+
+import {
+  Chart as ChartJS,
+  LineElement,
+  CategoryScale, // x axis
+  LinearScale, // y axis
+  PointElement,
+  Legend,
+  Tooltip,
+  Filler,
+} from "chart.js";
+
+ChartJS.register(
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  Legend,
+  Tooltip,
+  Filler
+);
+
+const salesData = [
+  { month: "January", sales: 100 },
+  { month: "February", sales: 150 },
+  { month: "March", sales: 200 },
+  { month: "April", sales: 120 },
+  { month: "May", sales: 180 },
+  { month: "June", sales: 250 },
+];
+
+function LineChart() {
+  const data = {
+    labels: salesData.map((data) => data.month),
+    datasets: [
+      {
+        label: "Revenue",
+        data: salesData.map((data) => data.sales),
+        borderColor: "#cb0c9f",
+        borderWidth: 3,
+        pointBorderColor: "#cb0c9f",
+        pointBorderWidth: 3,
+        tension: 0.5,
+        fill: true,
+        backgroundColor: (context) => {
+          const ctx = context.chart.ctx;
+          const gradient = ctx.createLinearGradient(0, 0, 0, 300);
+          gradient.addColorStop(0, "#f797e1");
+          gradient.addColorStop(1, "white");
+          return gradient;
+        },
+      },
+    ],
+  };
+
+  const options = {
+    plugins: {
+      legend: true,
+    },
+    responsive: true,
+    scales: {
+      y: {
+        ticks: {
+          font: {
+            size: 17,
+            weight: "bold",
+          },
+        },
+        title: {
+          display: true,
+          text: "Sales",
+          padding: {
+            bottom: 10,
+          },
+          font: {
+            size: 30,
+            style: "italic",
+            family: "Arial",
+          },
+        },
+        min: 50,
+      },
+      x: {
+        ticks: {
+          font: {
+            size: 17,
+            weight: "bold",
+          },
+        },
+        title: {
+          display: true,
+          text: "Month",
+          padding: {
+            top: 10,
+          },
+          font: {
+            size: 30,
+            style: "italic",
+            family: "Arial",
+          },
+        },
+      },
+    },
+  };
+
+  return (
+    <div>
+      <h1 className="font-bold text-3xl text-center mt-10">
+        Line Chart using ChartJS
+      </h1>
+      <div
+        style={{
+          width: "900px",
+          height: "400px",
+          padding: "20px",
+          cursor: "pointer",
+        }}
+      >
+        <Line data={data} options={options}></Line>
+      </div>
+    </div>
+  );
+}
+
+export default LineChart;
